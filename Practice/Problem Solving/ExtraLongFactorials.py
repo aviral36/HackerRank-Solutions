87d4c88aed75feb6af3e54b2cd9e3042e425b5ce
Aviral Sharma <aviral-s@outlook.com>
2018-07-18 15:54:02 +0530

    Update ExtraLongFactorials.py

--- Practice/Problem Solving/ExtraLongFactorials.py
+++ Practice/Problem Solving/ExtraLongFactorials.py
@@ -6,32 +6,10 @@ import random
 import re
 import sys
 
-def find(arr,x):
-    #finds index of element x
-    i = arr.index(x)
-    return i 
-
-# Complete the permutationEquation function below.
-def permutationEquation(p):
-    l = len(p)
-    result = list()
-    for x in range(1,l+1):
-        y = find(p,x) + 1
-        z = find(p,y) + 1
-        result.append(z)
-    return result
-    
-
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    print(math.factorial(n))
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
 
-    p = list(map(int, input().rstrip().split()))
-
-    result = permutationEquation(p)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
+    extraLongFactorials(n)
