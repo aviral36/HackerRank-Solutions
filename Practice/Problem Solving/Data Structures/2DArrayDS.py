b6548647e4c15403ee43bc70c8ed74a862bb7025
Aviral Sharma <aviral-s@outlook.com>
2018-09-16 19:28:33 +0530

    Create 2DArrayDS.py

---
+++ Practice/Problem Solving/Data Structures/2DArrayDS.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+import sys
+
+
+arr = []
+for arr_i in xrange(6):
+    arr_temp = map(int,raw_input().strip().split(' '))
+    arr.append(arr_temp)
+sumArr=[]
+for i in range(4):
+    for j in range(4):
+        tempsum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+        sumArr.append(tempsum)
+print max(sumArr)
