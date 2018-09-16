31e85df2c9b51bd088dbf27d6349daa9e1386a1b
Aviral Sharma <aviral-s@outlook.com>
2018-09-16 18:59:55 +0530

    Create SetsSTL.cpp

---
+++ Practice/CPP/SetsSTL.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int q;
+    set<int> s;
+    cin>>q;
+    for(int i=0;i<q;i++){
+        int a,b;
+        cin>>a>>b;
+        if(a==1){
+            s.insert(b);
+        }
+        else if(a==2){
+            s.erase(b);
+        }
+        else{
+            set<int>::iterator itr = s.find(b);
+            if(itr==s.end())
+                cout<<"No"<<endl;
+            else{
+                cout<<"Yes"<<endl;
+            }
+        }
+    }
+    return 0;
+}
+
+
+
